1c6749a7da34f4281214deaa4d852bb3fadf73e4
Mamut <komekbaevmamut06@gmail.com>
2022-11-07 19:28:43 +0300

    mamut

--- main/urls.py
+++ main/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('onlinestoresecret/', admin.site.urls),
     path('',include('core.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
