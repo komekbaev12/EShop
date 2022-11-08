ebeb5ed253bff5b70dac24947d0a3a370f5a8a76
Mamut <komekbaevmamut06@gmail.com>
2022-11-08 13:08:57 +0300

    Second

--- main/settings.py
+++ main/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iq2y@z#0x#!e!v^xkw@9025hvq4o@!3o40u_4xz4p1z_%e1=x$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'core',
-    
+
 
     # Download apps
     'ckeditor',
